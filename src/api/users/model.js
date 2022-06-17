bff80ac0b34dd7947a91f81e3baf087f0f74135f
Lucai994 <luca.muratori.94@hotmail.it>
2022-06-17 16:22:13 +0200

    create the user model

--- src/api/users/model.js
+++ src/api/users/model.js
@@ -5,4 +5,60 @@ const { Schema, Model } = mongoose;
 
 const UserSchema = new Schema({
   name: { type: String, required: true },
+  surname: { type: String, required: true },
+  avatar: {
+    type: String,
+    default: "https://ui-avatars.com/api/?name=Unnamed+User",
+  },
+  whereUserLive: { type: String, required: true },
+  photos: {
+    type: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Photo",
+      },
+    ],
+  },
+  userToDoList: {
+    type: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "ToDo",
+      },
+    ],
+  },
+  favoritePhotos: {
+    type: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Photo",
+      },
+    ],
+  },
+  friends: {
+    type: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  favoritePlace: {
+    type: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Place",
+      },
+    ],
+  },
+  comments: {
+    type: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
 });
+
+export default mongoose.model("User", UserSchema);
