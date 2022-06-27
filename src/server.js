2813287391cee11c808e241934cb4c6126890786
Lucai994 <luca.muratori.94@hotmail.it>
2022-06-27 15:23:31 +0200

    commit

--- src/server.js
+++ src/server.js
@@ -26,6 +26,12 @@ let whitelist = [
   "https://solo-capstone.herokuapp.com",
 ];
 
+server.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 var corsOptions = {
   origin: function (origin, callback) {
     if (!origin || whitelist.indexOf(origin) !== -1) {
