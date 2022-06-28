01e765837e043ca85978efb4bdba5f5e74f5daed
Lucai994 <luca.muratori.94@hotmail.it>
2022-06-28 17:14:32 +0200

    commit

--- src/api/user/index.js
+++ src/api/user/index.js
@@ -30,10 +30,8 @@ userRouter.get(
     // the purpose of this endpoint is to receive a response from Google, execute the google callback function and then send a response back
     try {
       const { accessToken, refreshToken } = req.user;
-
-      res.redirect(
-        `${process.env.FE_URL}/user?accessToken=${accessToken}&refreshToken=${refreshToken}`
-      );
+      res.send({ accessToken, refreshToken });
+      res.redirect(`${process.env.FE_URL}/user/home`);
     } catch (error) {
       next(error);
     }
